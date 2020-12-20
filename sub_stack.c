05fa45d16f26b6ae3e4cc5b78356499c37ac6fd5
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:47:27 +0000

    Added a functions that subtracts the top element of the stack from the second top element of the stack

---
+++ sub_stack.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * sub_stack - subtracts the top two elements of the stack.
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void sub_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int number_of_nodes;
+
+	number_of_nodes = len_stack(*stack);
+	if (number_of_nodes >= 2)
+	{
+		tmp = *stack;
+		*stack = tmp->next;
+		(*stack)->n = (*stack)->n - tmp->n;
+		(*stack)->prev = NULL;
+		free(tmp);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
