3471e0daa1191398ab1ed499ada6c96456c02f84
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:44:20 +0000

    Added a function that prints all the values on the stack, starting from the top of the stack

---
+++ pall_stack.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * pall_stack - Print all of the data stack.
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void pall_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+
+	tmp = *stack;
+	if (*stack)
+	{
+		while (tmp)
+		{
+			printf("%d\n", tmp->n);
+			tmp = tmp->next;
+		}
+	}
+}
