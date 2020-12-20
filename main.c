e9f6397c9b7d3d59cb301f2f7c165d17f0406495
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:40:01 +0000

    Added a programm that interpreted Monty ByteCodes files

---
+++ main.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+/**
+ * main - Programm to execute command monty bytycodes
+ * @argc: Arguments count from the terminal
+ * @argv: Arguments array of strings from the terminal.
+ *
+ * Return: 0 for success and 1 when it fails.
+ */
+int main(int argc, char **argv)
+{
+	char *buffer = NULL, *monty_command = NULL;
+	unsigned int line_number = 1;
+	stack_t *head = NULL;
+	FILE *fp;
+	size_t len;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+	if (!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &len, fp) != -1)
+	{
+		buffer[strlen(buffer) - 1] = '\0';
+		monty_command = strtok(buffer, " ");
+		if (monty_command != NULL && strcmp(monty_command, "nop") != 0)
+			get_opcode(&head, monty_command, line_number);
+		line_number++;
+	}
+	fclose(fp);
+	return (0);
+}
