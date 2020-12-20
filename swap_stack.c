69f455d4b0a709d124f17b2340427f3a5bf5ec20
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:46:02 +0000

    Added a functions that swaps the top two elements of the stack

---
+++ swap_stack.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * swap_stack - swaps the top two elements of the stack.
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void swap_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int data, number_of_nodes;
+
+	number_of_nodes = len_stack(*stack);
+	if (number_of_nodes >= 2)
+	{
+		tmp = (*stack)->next;
+		data = tmp->n;
+		tmp->n = (*stack)->n;
+		(*stack)->n = data;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
