559b3ace4a2775517ca535ca0401e0343993c9fb
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:45:05 +0000

    Added a functions that prints the value at the top of the stack, followed by a new line

---
+++ pint_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * pint_stack - Print the top data of the stack.
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void pint_stack(stack_t **stack, unsigned int line_number)
+{
+	if (*stack)
+		printf("%d\n", (*stack)->n);
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
