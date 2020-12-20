0d6a366aedde5f0aca6744c7dfb1d0eec57c983b
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:41:52 +0000

    Added a function that pushes an element to the stack

---
+++ push_stack.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * push_stack - add node into a doubly linked list(stack).
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void push_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = NULL;
+	char *value;
+	int data;
+
+	value = strtok(NULL, " ");
+	data =  check_integer(value, line_number);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = data;
+	new_node->prev = NULL;
+	if (*stack == NULL)
+		new_node->next = NULL;
+	else
+	{
+		(*stack)->prev = new_node;
+		new_node->next = *stack;
+	}
+	*stack = new_node;
+}
