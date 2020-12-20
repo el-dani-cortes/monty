8c01b2c7db4530d4f7455f2f172436db962d7528
el-dani-cortes <2304@holbertonschool.com>
2020-12-20 13:48:24 +0000

    Added a function that multiplies the second top element of the stack with the top element of the stack

---
+++ mul_stack.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * mul_stack - Mulitiplies the top two elements of the stack.
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void mul_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int number_of_nodes;
+
+	number_of_nodes = len_stack(*stack);
+	if (number_of_nodes >= 2)
+	{
+		tmp = *stack;
+		*stack = tmp->next;
+		(*stack)->n = tmp->n * (*stack)->n;
+		(*stack)->prev = NULL;
+		free(tmp);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
