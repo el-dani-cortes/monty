7d7e1b3b26e7bbb3c6742c7b1b2adcae2a5c50b2
el-dani-cortes <2304@holbertonschool.com>
2020-12-21 19:53:01 +0000

    Added function queue_format and if condition to run when the opcode stack or queue is given

--- push_stack.c
+++ push_stack.c
@@ -12,6 +12,51 @@ void push_stack(stack_t **stack, unsigned int line_number)
 	char *value;
 	int data;
 
+	if (strcmp(vars.format, "queue") == 0)
+		queue_format(stack, line_number);
+	else
+	{
+		value = strtok(NULL, " ");
+		if (value == NULL)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			free_all(stack, vars.buffer, vars.fd);
+			exit(EXIT_FAILURE);
+		}
+		data =  check_integer(stack, value, line_number);
+		new_node = malloc(sizeof(stack_t));
+		if (!new_node)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			free_all(stack, vars.buffer, vars.fd);
+			exit(EXIT_FAILURE);
+		}
+		new_node->n = data;
+		new_node->prev = NULL;
+		if (*stack == NULL)
+			new_node->next = NULL;
+		else
+		{
+			(*stack)->prev = new_node;
+			new_node->next = *stack;
+		}
+		*stack = new_node;
+	}
+}
+/**
+ * queue_format - Function that adds a node at the end.
+ * @stack: Head pointer of the double linked list.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void queue_format(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = NULL;
+	stack_t *tmp = NULL;
+	int data;
+	char *value;
+
 	value = strtok(NULL, " ");
 	if (value == NULL)
 	{
@@ -20,6 +65,7 @@ void push_stack(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	data =  check_integer(stack, value, line_number);
+	tmp = *stack;
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
@@ -28,13 +74,20 @@ void push_stack(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	new_node->n = data;
-	new_node->prev = NULL;
 	if (*stack == NULL)
+	{
 		new_node->next = NULL;
+		new_node->prev = NULL;
+		*stack = new_node;
+	}
 	else
 	{
-		(*stack)->prev = new_node;
-		new_node->next = *stack;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		new_node->prev = tmp;
+		new_node->next = NULL;
+		tmp->next = new_node;
 	}
-	*stack = new_node;
 }
