ea5ce2c969e4a790294248bc1860ade281394e9a
el-dani-cortes <2304@holbertonschool.com>
2020-12-21 12:28:21 +0000

    Added a functionn that rotates the stack to the bottom

---
+++ rotr_stack.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * rotr_stack - Rotates the stack to the bottom
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void rotr_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	(void)line_number;
+
+	tmp = *stack;
+	if (*stack)
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->prev->next = NULL;
+		tmp->prev = NULL;
+		tmp->next = *stack;
+		(*stack)->prev = tmp;
+		*stack = tmp;
+	}
+}
