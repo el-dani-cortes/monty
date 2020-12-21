620f90cc3c052fce80758986db2f7124f6ad45ce
el-dani-cortes <2304@holbertonschool.com>
2020-12-21 03:03:54 +0000

    Added a function that prints the char according the integer in the top of the stack

---
+++ pchar_stack.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * pchar_stack - Prints the char at the top of the stack
+ * @stack: Pointer head of the double linkedlist.
+ * @line_number: line number of bytecode file.
+ *
+ * Return: Always nothing.
+ */
+void pchar_stack(stack_t **stack, unsigned int line_number)
+{
+	if (*stack)
+	{
+		if ((*stack)->n >= 0 && (*stack)->n <= 127)
+			printf("%c\n", (*stack)->n);
+		else
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range",
+				line_number);
+			free_all(stack, vars.buffer, vars.fd);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_all(stack, vars.buffer, vars.fd);
+		exit(EXIT_FAILURE);
+	}
+}
