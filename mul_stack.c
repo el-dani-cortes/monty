9224e7c38b04bfc39e76e22560efb56a52b07d57
el-dani-cortes <2304@holbertonschool.com>
2020-12-21 02:57:41 +0000

    Added a free all function to free all memory

--- mul_stack.c
+++ mul_stack.c
@@ -23,6 +23,7 @@ void mul_stack(stack_t **stack, unsigned int line_number)
 	else
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_all(stack, vars.buffer, vars.fd);
 		exit(EXIT_FAILURE);
 	}
 }
